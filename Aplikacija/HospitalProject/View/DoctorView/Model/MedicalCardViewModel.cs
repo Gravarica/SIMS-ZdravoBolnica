c9425e3b9a2d585e99948c7526edf08ad78f404c
Gravarica <93195018+Gravarica@users.noreply.github.com>
2022-04-23 02:10:49 +0200

    Added view models for patient information and anamnesis view

--- Aplikacija/HospitalProject/View/DoctorView/Model/MedicalCardViewModel.cs
+++ Aplikacija/HospitalProject/View/DoctorView/Model/MedicalCardViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalProject.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,49 @@ using System.Threading.Tasks;
 
 namespace HospitalProject.View.DoctorView.Model
 {
-    public class MedicalCardViewModel
+    public class MedicalCardViewModel : BaseViewModel
     {
+
+        public RelayCommand PatientInformationViewCommand { get; set; }
+
+        public RelayCommand PreviousExaminationViewCommand { get; set; }
+
+        public PatientInformationViewModel PatientInformationVM { get; set; }
+
+        public PreviousExaminationsViewModel PreviousExaminationsVM { get; set; }
+
+        private object _CurrentView;
+
+        public object CurrentView
+        {
+            get 
+            { 
+                return _CurrentView; 
+            }
+            set 
+            { 
+                _CurrentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MedicalCardViewModel()
+        {
+            PatientInformationVM = new PatientInformationViewModel();
+
+            PreviousExaminationsVM = new PreviousExaminationsViewModel();
+
+            PatientInformationViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PatientInformationVM;
+            });
+
+            PreviousExaminationViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PreviousExaminationsVM;
+            });
+        }
+
+
     }
 }
